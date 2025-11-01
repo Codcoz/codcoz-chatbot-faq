7ce99634e2a7e9ee254d6a107cf4df36096d4afe
Gabriel-Koji <gabriel.kojikondo@gmail.com>
2025-11-01 16:31:39 -0300

    Adding dockerfile to render

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from chatbot.routes import chatbot_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(chatbot_bp)  # expõe /chat e /health
+    app.register_blueprint(chatbot_bp)
     return app
 
-# exporta a instância para o Flask CLI quando FLASK_APP=app.py
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
